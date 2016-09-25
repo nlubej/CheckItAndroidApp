d50df21c265bfc2c315c4fe4647f9385be26659a
nlubej <neejc.lubej@gmail.com>
2016-09-25 14:24:10 +0200

    Preparing structure

--- CheckItAndroidApp/MainActivity.cs
+++ CheckItAndroidApp/MainActivity.cs
@@ -23,6 +23,8 @@ namespace CheckItAndroidApp
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
+
+            var m = IsChangingConfigurations
             // Get our button from the layout resource,
             // and attach an event to it
 
